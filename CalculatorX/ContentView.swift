b235ba62e346e24123f76b0233f87be148c37c7c
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-12-15 12:41:22 +0000

    first commit

--- CalculatorX/ContentView.swift
+++ CalculatorX/ContentView.swift
@@ -1,21 +1,15 @@
-//
-//  ContentView.swift
-//  CalculatorX
-//
-//  Created by Carlos dos Santos Filho on 15/12/2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Button("7") {
+                    
+                }
+                .background(Color.black)
+            }
         }
-        .padding()
     }
 }
 
