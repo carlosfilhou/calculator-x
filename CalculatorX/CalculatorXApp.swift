e87cb30213d8f603253d16871aaa26c3ce12dee1
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-12-15 11:39:43 +0000

    Initial Commit

---
+++ CalculatorX/CalculatorXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorXApp.swift
+//  CalculatorX
+//
+//  Created by Carlos dos Santos Filho on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalculatorX
+//
+//  Created by Carlos dos Santos Filho on 15/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
