d2b81e4e7db385a9a9079d3385ccc6dc3f011723
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-12-16 03:38:23 +0000

    add buttons

--- CalculatorX/ContentView.swift
+++ CalculatorX/ContentView.swift
@@ -1,15 +1,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let graphiteColor = Color(UIColor(red: 120/255, green: 123/255, blue: 118/255, alpha: 1.0))
+
+
     var body: some View {
-        VStack {
+        VStack(spacing: 1) {
             HStack {
                 Button("7") {
                     
                 }
-                .background(Color.red)
+                .padding()
+                .frame(maxWidth: .infinity)
+                Button("8") {
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                Button("9") {
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                Button("X") {
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.green)
+            }
+            
+            HStack {
+                Button("4") {
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                Button("5") {
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                Button("6") {
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                Button("-") {
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color(graphiteColor))
             }
         }
+        .background(Color(graphiteColor))
+        .foregroundColor(.white)
     }
 }
 
